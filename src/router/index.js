010cd371962b89b1a89ce7ba6b3e4b29d7ce594b
jvn <73175916+JevenDev@users.noreply.github.com>
2026-04-13 23:14:53 -0400

    Switch hosting to GitHub Pages with custom domain

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ const routes = [
 ]
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) return savedPosition
